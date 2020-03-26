f097fff63274e00e35dbc023a1761cde582b75b2
Fish <vincentvenhuizen1997@gmail.com>
2020-03-26 19:06:36 +0100

    Worked on deployment
    Added port to client app auth en user api

--- clientapp/src/api/AuthApi.js
+++ clientapp/src/api/AuthApi.js
@@ -1,24 +1,24 @@
-import axios from 'axios'
-axios.defaults.withCredentials = true
 require('dotenv').config()
 const adminport = process.env.ADMIN_PORT;
+import axios from 'axios'
+axios.defaults.withCredentials = true
 
 const AuthApi = {
 	login(user) {
 		return axios
-			.post('http://localhost:'+adminport+'/api/login', {
+			.post('http://localhost:' + adminport + '/api/login', {
 				email: user.email,
 				password: user.password,
 			}).then(response => response)
 	},
 	logout() {
 		return axios
-			.post('http://localhost:'+adminport+'/api/logout')
+			.post('http://localhost:' + adminport + '/api/logout')
 			.then(response => response.data)
 	},
 	register(data) {
 		return axios
-			.post('http://localhost:'+adminport+'/api/register', {
+			.post('http://localhost:' + adminport + '/api/register', {
 				email: data.email,
 				password: data.password,
 				nickname: data.nickname
--- clientapp/src/api/UserApi.js
+++ clientapp/src/api/UserApi.js
@@ -1,20 +1,22 @@
+require('dotenv').config()
+const adminport = process.env.ADMIN_PORT;
 import axios from 'axios'
 axios.defaults.withCredentials = true
 
 const UserApi = {
 	game_status() {
 		return axios
-		.get("http://localhost:3000/api/game_status")
-		.then(response => response.data)
+			.get('http://localhost:' + adminport + '/api/game_status')
+			.then(response => response.data)
 	},
 	leaderboard() {
 		return axios
-			.get('http://localhost:3000/api/leaderboard')
+			.get('http://localhost:' + adminport + '/api/leaderboard')
 			.then(response => response.data)
 	},
 	points() {
 		return axios
-			.get('http://localhost:3000/api/points')
+			.get('http://localhost:' + adminport + '/api/points')
 			.then(response => response.data)
 	}
 }
