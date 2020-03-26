9b4a518c9237b85183f8a065917220f269f377cf
Fish <vincentvenhuizen1997@gmail.com>
2020-03-26 17:31:50 +0100

    Worked on deployment
    Trying to fix admin...

--- adminpanel/Dockerfile
+++ adminpanel/Dockerfile
@@ -1,6 +1,6 @@
 # build stage
 FROM node:lts-alpine as build-stage
-WORKDIR /
+WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
--- adminpanel/src/index.js
+++ adminpanel/src/index.js
@@ -36,6 +36,7 @@ app.set('view engine', 'pug')
 app.set('public', path.join(__dirname, 'front-end/public'));
 app.set('views', path.join(__dirname, 'front-end/views'));
 app.use(express.static(path.join(__dirname, '/front-end/public')));
+app.use(express.static(path.join(__dirname, '/front-end/views')));
 app.locals.basedir = path.join(__dirname, 'front-end/public');
 
 // admin routes
