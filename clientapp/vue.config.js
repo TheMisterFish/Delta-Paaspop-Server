82cc5c45d4e38451ce0297e0140a468cc9f47933
Fish <vincentvenhuizen1997@gmail.com>
2020-03-26 14:42:23 +0100

    Worked on deployment
    Added a config for client app since I get a bad gateway

--- clientapp/vue.config.js
+++ clientapp/vue.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/assets/styles/_variables.scss";`
-      }
-    }
-  }
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: `@import "@/assets/styles/_variables.scss";`
+			}
+		}
+	},
+	devServer: {
+		disableHostCheck: true
+	}
 };
