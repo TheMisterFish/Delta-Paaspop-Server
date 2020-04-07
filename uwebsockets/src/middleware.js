f87f90c59612d1dda9305af9e6bdbbae98146498
Fish <vincentvenhuizen1997@gmail.com>
2020-04-07 12:24:28 +0200

    Update websocket game join bug

--- uwebsockets/src/middleware.js
+++ uwebsockets/src/middleware.js
@@ -32,15 +32,19 @@ module.exports = {
 		});
 	},
 	ws_is_game: function (ws, client) {
+		if (client['sec-websocket-protocol'] == undefined) {
+			return false;
+		}
+		let token;
 		try {
 			token = client['sec-websocket-protocol'].split(":");
+			if (token[0] == "token" && token[1] == game_token) {
+				return true
+			}
+			return false;
 		} catch (error) {
 			return false;
 		}
-		if (token[0] == "token" && token[1] == game_token) {
-			return true
-		}
-		return false;
 	},
 	game_running: async function () {
 		return await storage.get_value('game_name').then((value) => {
