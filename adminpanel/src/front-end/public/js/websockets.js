2b3ecf4a47c467936451ab68bc22160273a58852
Fish <vincentvenhuizen1997@gmail.com>
2020-04-09 19:10:32 +0200

    Trying to fix localhost error

--- adminpanel/src/front-end/public/js/websockets.js
+++ adminpanel/src/front-end/public/js/websockets.js
@@ -3,47 +3,46 @@ var route = document.getElementById('route').innerText;
 var textbox = document.getElementById('socketChannel');
 var message = document.getElementById('message');
 
-let socket = new WebSocket('ws://127.0.0.1:9000', ["token", token]);
+let socket = new WebSocket('ws://' + location.host + ': 9000 ', ["token", token]);
 
-var input = document.getElementById("message");
+		var input = document.getElementById("message");
 
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("sendbutton").click();
-  }
-}); 
+		input.addEventListener("keyup", function (event) {
+			if (event.keyCode === 13) {
+				event.preventDefault();
+				document.getElementById("sendbutton").click();
+			}
+		});
 
-socket.onopen = function (e) {
-	addText("[status] Connected to ws://127.0.0.1:9000");
-};
-socket.onmessage = function (event) {
-	addText(`[message] Data received from server: ${event.data}`);
-};
+		socket.onopen = function (e) {
+			addText("[status] Connected to ws://" + location.host + ":9000");
+		}; socket.onmessage = function (event) {
+			addText(`[message] Data received from server: ${event.data}`);
+		};
 
-socket.onclose = function (event) {
-	if (event.wasClean) {
-		addText(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-	} else {
-		addText('[close] Connection died');
-	}
-};
+		socket.onclose = function (event) {
+			if (event.wasClean) {
+				addText(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+			} else {
+				addText('[close] Connection died');
+			}
+		};
 
-socket.onerror = function (error) {
-	addText(`[error] ${error.message}`);
-};
+		socket.onerror = function (error) {
+			addText(`[error] ${error.message}`);
+		};
 
-function addText(message) {
-	var d = new Date();
-	var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" +
-		d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
+		function addText(message) {
+			var d = new Date();
+			var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" +
+				d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
 
-	textbox.value += datestring + " " + message + '\n';
-	textbox.scrollTop = textbox.scrollHeight;
-}
+			textbox.value += datestring + " " + message + '\n';
+			textbox.scrollTop = textbox.scrollHeight;
+		}
 
-function sendText() {
-	socket.send(message.value);
-	addText('[sending] ' + message.value);
-	message.value = "";
-}
+		function sendText() {
+			socket.send(message.value);
+			addText('[sending] ' + message.value);
+			message.value = "";
+		}
